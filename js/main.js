7a19110e9b67962967e7d18d9f64c7028b875bd9
alondramora <mora_ac@yahoo.com>
2022-04-17 19:54:53 -0700

    updated styling

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.querySelector('button').addEventListener('click', getHoroscope)
+
+function getHoroscope() {
+    const sign = document.querySelector('input').value.toLowerCase()
+    const url = `https://aztro.sameerkumar.website/?sign=${sign}`;
+
+    fetch(url, {
+        method: 'POST'
+    })
+        .then(res => res.json())
+        .then(data => {
+            const description = data.description
+            const dateRange = data.date_range
+            const luckyColor = data.color
+            const mood = data.mood
+            if (sign === "aries" || sign === "taurus" || sign === "gemini" || sign === "cancer" || sign === "leo" || sign === "virgo" || sign === "libra" || sign === "scorpio" || sign === "sagittarius" || sign === "capricorn" || sign === "aquarius" || sign === "pisces") {
+                document.querySelector('#zodiac-sign').innerText = sign
+                document.querySelector('p').innerText = description
+                document.querySelector('#date-range').innerText = dateRange
+                document.querySelector('#lucky-color').innerText = `Lucky Color: ${luckyColor}`
+                document.querySelector('#mood').innerText = `Feels: ${mood}`
+            } else {
+                document.querySelector('#zodiac-sign').innerText = 'Choose your sign.'
+                document.querySelector('p').innerText = ' '
+                document.querySelector('#date-range').innerText = 'Date Range'
+                document.querySelector('#lucky-color').innerText = 'Lucky Color'
+                document.querySelector('#mood').innerText = 'Feels'
+
+            };
+  
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
